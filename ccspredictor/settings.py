074bea39c727308970612c475a2528136ba2130a
Amir Aghajan <133090619+AmirAg47@users.noreply.github.com>
2026-02-13 17:15:33 +0330

    Update settings.py
    http

--- ccspredictor/settings.py
+++ ccspredictor/settings.py
@@ -14,6 +14,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 21536000  # یک سال
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_ROOT = BASE_DIR / 'media'
