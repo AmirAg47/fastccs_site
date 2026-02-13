107715450ad534c42d726732e5e50872bfb8e2e0
Amir Aghajan <133090619+AmirAg47@users.noreply.github.com>
2026-02-13 19:54:19 +0330

    Update settings.py
    site

--- ccspredictor/settings.py
+++ ccspredictor/settings.py
@@ -70,7 +70,7 @@ DEBUG = True # تغییرش دادم
 CELERY_BROKER_URL = "redis://fastccs1-yqq-service:6379/0"
 CELERY_BROKER_PASSWORD = 'h4POa4K8XwfzSYlZjnbu'
 
-ALLOWED_HOSTS = ['fastccs.com', 'www.fastccs.com', 'https://fastccs.com' , 'fastccs.runflare.run', 'www.fastccs.runflare.run']
+ALLOWED_HOSTS = ['fastccs.com', 'www.fastccs.com', 'https://fastccs.com' , 'fastccs.runflare.run', 'www.fastccs.runflare.run', 'https://www.fastccs.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
