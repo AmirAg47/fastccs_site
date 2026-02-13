36644aec736db8705e5c2a87503f3e3a6a21a732
Amir Aghajan <133090619+AmirAg47@users.noreply.github.com>
2026-02-13 16:42:47 +0330

    Update urls.py
    http

--- predictor/urls.py
+++ predictor/urls.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect 
 from . import views
+from django.http import HttpResponse
+
 
 urlpatterns = [
     path('predict/', views.predict, name='predict'),
@@ -16,3 +18,14 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+from django.http import HttpResponse
+
+def certum_verify(request):
+    return HttpResponse(
+        "99e5f8d60e442a2e2071634522399e2886d828516ec5afe17d29d11493a44b8-certum.pl",
+        content_type="text/plain"
+    )
+
+urlpatterns += [
+    path('.well-known/pki-validation/certum.txt', certum_verify),
+]
